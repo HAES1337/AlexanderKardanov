aa66ffe1a6d1c02769a1453c329ed4340f9907db
HAES1337 <86319274+HAES1337@users.noreply.github.com>
2021-09-01 16:36:56 +0300

    Collection fix

--- src/test/java/com/epam/tc/hw2/ex1/LoginPageTest.java
+++ src/test/java/com/epam/tc/hw2/ex1/LoginPageTest.java
@@ -1,6 +1,8 @@
 package com.epam.tc.hw2.ex1;
 
 import com.epam.tc.hw2.AbstractBaseTest;
+import java.util.ArrayList;
+import java.util.List;
 import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -37,49 +39,35 @@ public class LoginPageTest extends AbstractBaseTest {
         webDriverWait.until(ExpectedConditions.textToBePresentInElement(userNameLogged, "ROMAN IOVLEV"));
         softly.assertThat(userNameLogged.getText()).isEqualTo("ROMAN IOVLEV");
         //Assert that there are 4 items on the header section are displayed and they have proper texts
-        WebElement homeOnTheHeader = webDriver.findElement(By.xpath("//a[@href='index.html']"));
-        homeOnTheHeader.isDisplayed();
-        WebElement contactsOnTheHeader = webDriver.findElement(By.xpath("//a[@href='contacts.html']"));
-        contactsOnTheHeader.isDisplayed();
-        WebElement serviceOnTheHeader = webDriver.findElement(By.cssSelector(".dropdown"));
-        serviceOnTheHeader.isDisplayed();
-        WebElement metalsColors = webDriver.findElement(By.xpath("//a[@href='metals-colors.html']"));
-        metalsColors.isDisplayed();
-        softly.assertThat(homeOnTheHeader.getText()).isEqualTo("HOME");
-        softly.assertThat(contactsOnTheHeader.getText()).isEqualTo("CONTACT FORM");
-        softly.assertThat(serviceOnTheHeader.getText()).isEqualTo("SERVICE");
-        softly.assertThat(metalsColors.getText()).isEqualTo("METALS & COLORS");
+        List<WebElement> headerMenuWebElements = webDriver.findElements(By.xpath("//ul[contains(@class,'uui-navigation nav navbar-nav m-')]/li/a"));
+        headerMenuWebElements.get(0).isDisplayed();
+        headerMenuWebElements.get(1).isDisplayed();
+        headerMenuWebElements.get(2).isDisplayed();
+        headerMenuWebElements.get(3).isDisplayed();
+        softly.assertThat(headerMenuWebElements.get(0).getText()).isEqualTo("HOME");
+        softly.assertThat(headerMenuWebElements.get(1).getText()).isEqualTo("CONTACT FORM");
+        softly.assertThat(headerMenuWebElements.get(2).getText()).isEqualTo("SERVICE");
+        softly.assertThat(headerMenuWebElements.get(3).getText()).isEqualTo("METALS & COLORS");
         //Assert that there are 4 images on the Index Page and they are displayed
-        webDriver.findElement(By.xpath("//img[contains(@id,'epam-logo')]")).isDisplayed();
-        webDriver.findElement(By.xpath("//img[contains(@id,'user-icon')]")).isDisplayed();
-        WebElement icon1 = webDriver.findElement(By.className("icon-practise"));
-        icon1.isDisplayed();
-        WebElement icon2 = webDriver.findElement(By.className("icon-custom"));
-        icon2.isDisplayed();
-        WebElement icon3 = webDriver.findElement(By.className("icon-multi"));
-        icon3.isDisplayed();
-        WebElement icon4 = webDriver.findElement(By.className("icon-base"));
-        icon4.isDisplayed();
+        List<WebElement> iconsWebElements = webDriver.findElements(By.className("benefit-icon"));
+        iconsWebElements.get(0).isDisplayed();
+        iconsWebElements.get(1).isDisplayed();
+        iconsWebElements.get(2).isDisplayed();
+        iconsWebElements.get(3).isDisplayed();
         //Assert that there are 4 texts on the Index Page under icons and they have proper text
-        WebElement textUnder1 =
-            webDriver.findElement(By.xpath("//div[@class='col-sm-3']//*[contains(text(),'To include')]"));
-        textUnder1.isDisplayed();
-        softly.assertThat(textUnder1.getText()).isEqualTo("To include good practices\n"
+        List<String> allTextsUnderIcon = new ArrayList<>();
+        List<WebElement> textUnderWebElements =
+            webDriver.findElements(By.className("benefit-txt"));
+        for (WebElement webElement : textUnderWebElements) {
+            allTextsUnderIcon.add(webElement.getText());
+        }
+        softly.assertThat(allTextsUnderIcon.get(0)).isEqualTo("To include good practices\n"
             + "and ideas from successful\n"
             + "EPAM project");
-        WebElement textUnder2 =
-            webDriver.findElement(By.xpath("//div[@class='col-sm-3']//*[contains(text(),'To be flexible')]"));
-        textUnder2.isDisplayed();
-        softly.assertThat(textUnder2.getText()).isEqualTo("To be flexible and\n"
+        softly.assertThat(allTextsUnderIcon.get(1)).isEqualTo("To be flexible and\n"
             + "customizable");
-        WebElement textUnder3 =
-            webDriver.findElement(By.xpath("//div[@class='col-sm-3']//*[contains(text(),'To be multiplatform')]"));
-        textUnder3.isDisplayed();
-        softly.assertThat(textUnder3.getText()).isEqualTo("To be multiplatform");
-        WebElement textUnder4 =
-            webDriver.findElement(By.xpath("//div[@class='col-sm-3']//*[contains(text(),'Already have good base')]"));
-        textUnder4.isDisplayed();
-        softly.assertThat(textUnder4.getText()).isEqualTo("Already have good base\n"
+        softly.assertThat(allTextsUnderIcon.get(2)).isEqualTo("To be multiplatform");
+        softly.assertThat(allTextsUnderIcon.get(3)).isEqualTo("Already have good base\n"
             + "(about 20 internal and\n"
             + "some external projects),\n"
             + "wish to get more…");
@@ -93,24 +81,17 @@ public class LoginPageTest extends AbstractBaseTest {
         //Switch to original window back
         webDriver.switchTo().defaultContent();
         //Assert that there are 5 items in the Left Section are displayed and they have proper text
-        WebElement homeOnLeftSection =
-            webDriver.findElement(By.xpath("//*[@class='sidebar-menu left']//a[@href='index.html']"));
-        homeOnLeftSection.isDisplayed();
-        softly.assertThat(homeOnLeftSection.getText()).isEqualTo("Home");
-        WebElement contactOnLeftSection =
-            webDriver.findElement(By.xpath("//*[@class='sidebar-menu left']//a[@href='contacts.html']"));
-        contactOnLeftSection.isDisplayed();
-        softly.assertThat(contactOnLeftSection.getText()).isEqualTo("Contact form");
-        WebElement serviceOnLeftSection = webDriver.findElement(By.xpath("//span[contains(text(),'Service')]"));
-        serviceOnLeftSection.isDisplayed();
-        softly.assertThat(serviceOnLeftSection.getText()).isEqualTo("Service");
-        WebElement metalsAndColorsOnLeftSection =
-            webDriver.findElement(By.xpath("//*[@class='sidebar-menu left']//a[@href='metals-colors.html']"));
-        serviceOnLeftSection.isDisplayed();
-        softly.assertThat(metalsAndColorsOnLeftSection.getText()).isEqualTo("Metals & Colors");
-        WebElement elementPacksOnLeftSection = webDriver.findElement(By.xpath("//span[contains(text(),'Element')]"));
-        elementPacksOnLeftSection.isDisplayed();
-        softly.assertThat(elementPacksOnLeftSection.getText()).isEqualTo("Elements packs");
+        List<WebElement> leftSideMenuWebElements = webDriver.findElements(By.xpath("//*[@class='sidebar-menu left']/li[@index]"));
+        leftSideMenuWebElements.get(0).isDisplayed();
+        leftSideMenuWebElements.get(1).isDisplayed();
+        leftSideMenuWebElements.get(2).isDisplayed();
+        leftSideMenuWebElements.get(3).isDisplayed();
+        leftSideMenuWebElements.get(4).isDisplayed();
+        softly.assertThat(leftSideMenuWebElements.get(0).getText()).isEqualTo("Home");
+        softly.assertThat(leftSideMenuWebElements.get(1).getText()).isEqualTo("Contact form");
+        softly.assertThat(leftSideMenuWebElements.get(2).getText()).isEqualTo("Service");
+        softly.assertThat(leftSideMenuWebElements.get(3).getText()).isEqualTo("Metals & Colors");
+        softly.assertThat(leftSideMenuWebElements.get(4).getText()).isEqualTo("Elements packs");
         softly.assertAll();
     }
 }
